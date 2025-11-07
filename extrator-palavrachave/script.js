00eb61d52d01b53235fe7572fb606b1db947c105
daf135 <dafne.wilbert@escola.pr.gov.br>
2025-11-07 10:20:53 -0300

    Add files via upload

--- extrator-palavrachave/script.js
+++ extrator-palavrachave/script.js
@@ -1,7 +1,16 @@
-const botaoMostraPalavras = document.queryselector('#botao-palavrachave');
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave(){
-    alert('Fui clicado!')
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavras = processaTexto(texto);
+    
+    campoResultado.textContent = palavras.join(", ");
+}
+
+function processaTexto(texto) {
+    let palavraS = texto.split(" ");
+    return palavras;
 }
